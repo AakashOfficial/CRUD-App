d2b3ef51edc5802512b97421031dc9d764f31049
AakashOfficial <aakashtyagi1996@outlook.com>
2020-01-30 14:07:53 +0530

    Create Action

---
+++ CRUD App/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using CRUD_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_App.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
